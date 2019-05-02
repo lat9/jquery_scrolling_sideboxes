efb76500075bec1b4a33537c69ac1a8967d8476d
lat9 <lat9@vinosdefrutastropicales.com>
2019-05-02 15:40:27 -0400

    #8: Correct specials template issue

--- includes/templates/template_default/sideboxes/tpl_specials_scrolling.php
+++ includes/templates/template_default/sideboxes/tpl_specials_scrolling.php
@@ -13,7 +13,7 @@ while (!$scrolling_specials->EOF) {
     $ss_pname = $scrolling_specials->fields['products_name'];
     $specials_box_price = zen_get_products_display_price($ss_pid);
     $content .= "\n" . '  <div class="sideBoxContentItem hiddenField specialsScroller">';
-    $content .= '<a href="' . zen_href_link(zen_get_info_page($ss_pid), 'cPath=' . zen_get_generated_category_path_rev($scrolling_specials->fields["master_categories_id"]) . "&products_id=$ss_pid"]) . '">' . zen_image(DIR_WS_IMAGES . $scrolling_specials->fields['products_image'], $ss_pname, SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT);
+    $content .= '<a href="' . zen_href_link(zen_get_info_page($ss_pid), 'cPath=' . zen_get_generated_category_path_rev($scrolling_specials->fields['master_categories_id']) . "&products_id=$ss_pid") . '">' . zen_image(DIR_WS_IMAGES . $scrolling_specials->fields['products_image'], $ss_pname, SMALL_IMAGE_WIDTH, SMALL_IMAGE_HEIGHT);
     $content .= '<br />' . $ss_pname . '</a>';
     $content .= '<div>' . $specials_box_price . '</div>';
     $content .= '</div>';
